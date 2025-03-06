14f6592c86e83567e45050e0ec20160ccfcef348
Svendcorp <Buch_magnus@hotmail.com>
2025-03-06 09:33:51 +0100

    Projekt CarApp
    Vores nye projekt

---
+++ Projekt CarApp/Projekt CarApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_CarApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
